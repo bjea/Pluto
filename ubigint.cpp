f7ef801fdf2026cb8c875db6f91197cc05b9e25b
Bettie Jea <bjea@ucsc.edu>
2016-01-16 02:31:51 -0800

    ubigint + - *2 modified

--- ubigint.cpp
+++ ubigint.cpp
@@ -4,38 +4,151 @@
 #include <exception>
 #include <stack>
 #include <stdexcept>
+#include <string>
 using namespace std;
 
 #include "ubigint.h"
 #include "debug.h"
+/*
+ubigint::ubigint (unsigned long that): ubig_value (that) {
+   DEBUGF ('~', this << " -> " << ubig_value)
+}**/
 
-ubigint::ubigint (unsigned long that): uvalue (that) {
-   DEBUGF ('~', this << " -> " << uvalue)
-}
-
-ubigint::ubigint (const string& that): uvalue(0) {
-   for (char digit: that) uvalue = uvalue * 10 + digit - '0';
+ubigint::ubigint (const string& that) {
+   // Use const_reverse_iterator crbegin(), crend() to scan a string
+   // from the end of the string (least significant digit) to the
+   // beginning of the string (most significant digit).
+   for (auto rev_that = that.crbegin(); rev_that != that.crend(); ++rev_that)
+   {
+      ubig_value.push_back(*rev_that);
+   }
 }
 
 ubigint ubigint::operator+ (const ubigint& that) const {
-   return ubigint (uvalue + that.uvalue);
+   // = ubig_value * 10 + digit - '0';
+   ubigint result;
+   if (this->ubig_value.size() > that.ubig_value.size() )
+   {
+      int carry = 0;
+      for (int i = 0; i < this->ubig_value.size(); ++i)
+      {
+
+         if ((carry + this->ubig_value[i] - '0' + that.ubig_value[i] - '0') >= 10)
+         {
+            result.ubig_value.push_back(carry + this->ubig_value[i] - '0' + that.ubig_value[i] - '0' - 10);
+            carry = 1;
+         }
+         else
+         {
+            result.ubig_value.push_back(this->ubig_value[i] - '0' + that.ubig_value[i] - '0');
+            carry = 0;
+         }
+      }
+   }
+   else
+   {
+      int carry = 0;
+      for (int i = 0; i < that.ubig_value.size(); ++i)
+      {
+
+         if ((carry + this->ubig_value[i] - '0' + that.ubig_value[i] - '0') >= 10)
+         {
+
+            result.ubig_value.push_back(carry + this->ubig_value[i] - '0' + that.ubig_value[i] - '0' - 10);
+            carry = 1;
+         }
+         else
+         {
+            result.ubig_value.push_back(this->ubig_value[i] - '0' + that.ubig_value[i] - '0');
+            carry = 0;
+         }
+      }
+   }
+   //for (char c : ubig_value)
+
+   return result;
 }
 
 ubigint ubigint::operator- (const ubigint& that) const {
-   if (*this < that) throw domain_error ("ubigint::operator-(a<b)");
-   return ubigint (uvalue - that.uvalue);
+   if (this->ubig_value.size() < that.ubig_value.size())
+   {
+      throw domain_error ("ubigint::operator-(a<b)");
+   }
+   // How to compare the value of this & that
+   ubigint result;
+   int borrow = 0;
+   for (int i = 0; i < this->ubig_value.size(); ++i)
+   {
+
+      if ((this->ubig_value[i] - '0' - that.ubig_value[i] - '0' - borrow) < 0)
+      {
+
+         result.ubig_value.push_back(this->ubig_value[i] - '0' - borrow + 10 - that.ubig_value[i] - '0');
+         borrow = 1;
+      }
+      else
+      {
+         result.ubig_value.push_back(this->ubig_value[i] - '0' - that.ubig_value[i] - '0');
+         borrow = 0;
+      }
+   }
+
+   return result;
 }
 
 ubigint ubigint::operator* (const ubigint& that) const {
-   return ubigint (uvalue * that.uvalue);
+   ubigint result;
+   int carry_multiply = 0;
+   int carry_add = 0;
+   for (int i = 0; i < this->ubig_value.size(); ++i)
+   {
+      for (int j = 0; j < that.ubig_value.size(); ++j)
+      {
+         if (((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) >= 10)
+         {
+            carry_multiply = ((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) / 10;
+            if ((result.ubig_value[i+j] + (((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) % 10)) >= 10)
+            {
+               carry_add = (result.ubig_value[i+j] + (((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) % 10)) / 10;
+               result.ubig_value[i+j] += (((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) % 10);
+            }
+            else
+            {
+               result.ubig_value[i+j] += (((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply) % 10);
+            }
+         }
+         else
+         {
+            result.ubig_value[i+j] += ((this->ubig_value[i] - '0') * (that.ubig_value[j] - '0') + carry_multiply);
+         }
+      }
+   }
+
+   return result;
 }
 
 void ubigint::multiply_by_2() {
-   uvalue *= 2;
+   //ubigint result;
+
+      int carry = 0;
+      for (int i = 0; i < this->ubig_value.size(); ++i)
+      {
+
+         if ((carry + (this->ubig_value[i] - '0') * 2) >= 10)
+         {
+            carry = 1;
+            ubig_value[i] = (this->ubig_value[i] - '0') * 2 + carry - 10;
+         }
+         else
+         {
+            ubig_value[i] = (this->ubig_value[i] - '0') * 2;
+         }
+      }
+
 }
 
 void ubigint::divide_by_2() {
-   uvalue /= 2;
+   ubig_value /= 2;
 }
 
 
@@ -70,14 +183,14 @@ ubigint ubigint::operator% (const ubigint& that) const {
 }
 
 bool ubigint::operator== (const ubigint& that) const {
-   return uvalue == that.uvalue;
+   return ubig_value == that.ubig_value;
 }
 
 bool ubigint::operator< (const ubigint& that) const {
-   return uvalue < that.uvalue;
+   return ubig_value < that.ubig_value;
 }
 
 ostream& operator<< (ostream& out, const ubigint& that) { 
-   return out << "ubigint(" << that.uvalue << ")";
+   return out << "ubigint(" << that.ubig_value << ")";
 }
 
--- ubigint.h
+++ ubigint.h
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <limits>
 #include <utility>
+#include <vector>
 using namespace std;
 
 #include "debug.h"
@@ -16,8 +17,11 @@ class ubigint {
    friend ostream& operator<< (ostream&, const ubigint&);
    private:
       using quot_rem = pair<ubigint,ubigint>;
-      using unumber = unsigned long;
-      unumber uvalue {};
+      //using unumber = unsigned long;
+      //unumber uvalue {};
+      using udigit_t = unsigned char;
+      using ubigvalue_t = vector<udigit_t>;
+      ubigvalue_t ubig_value;
       quot_rem divide (const ubigint&) const;
       void multiply_by_2();
       void divide_by_2();
